c3b6d27eee493e2e2afe748e6cf02940b6c7b5f7
mparrishFAMU <61263623+mparrishFAMU@users.noreply.github.com>
2020-02-28 17:15:02 -0500

    Update B2BDriver_Parrish.cpp

--- B2BDriver_Parrish.cpp
+++ B2BDriver_Parrish.cpp
@@ -1,5 +1,15 @@
-#include <ctime>
-#include <sstream>
+/*
+B2BDriver.cpp
+Group Number: 6
+RaCaria Burgess, Michael Parrish, Janei Elliston, Michael Mondelice
+2/27/20
+The purpose of this Driver file is to incorporate the three class files together. As the associate enters the items (using the item number) and quantities, 
+the system should verify that the customer still has available credit and the stock has not be depleted. The system should also verify that the a
+ssociate is entering accurate information. 
+Once completed, the system will display a final order summary and update the stock of the items sold.
+ */
+
+
 #include <iostream> 
 #include <cstdlib>
 #include <vector>
@@ -14,34 +24,42 @@
 
 using namespace std;
 
-int fileCount;
-Customer *custId =0;
-Address *addressId =0;
-Product *prodId =0;
-
-void setPointers(int fileC){
-    if (custId !=0){
-        delete [] custId;
+//ALLOWS PROGRAM TO BE ABLE TO COUNT LINES IN THE FILES 
+int countFileLines(string holder)
+{
+    int count1=0;
+    string cust;
+    
+    fstream inFile;
+    inFile.open(holder);
+       while (getline(inFile, cust)){//creates a count of items to create pointer array
+        count1++;
+       }
+        return count1;
+    inFile.close();
+    
     }
-    if (addressId !=0){
-        delete [] addressId;
-    }
-    custId = new Customer[fileC];
-    addressId = new Address[fileC];
-}
-
-
+//INCREASE FILE COMPACITY AS IT GROWS
+int fileCount = countFileLines("customers.dat");
 //GLOBAL VARIABLES
+
 //ADDRESS
-string addy, cust, _streetAddress, _state, _zipCode, _addy,_city;
+string addy, cust, _streetAddress, _state, _zipCode, _addy, _city;
 //CUSTOMER
 string CName, CNum;
 double CreditL;
+
+//OBJECTS
+Customer custId[100];
+Address addressId[100];
+Product prodId[100];
+
 
 //MIRROR ADDRESS CLASS VECTOR 
 vector<int> mAddress;
 //ADDRESS VECTOR
 vector<Address> addVec;
+vector<Product>prodVec;
 
 //GENERATE RANDOM ORDER NUM
 string generateOrderNum(){ //orderSummary << generateOrderNum();
@@ -52,27 +70,13 @@ string generateOrderNum(){ //orderSummary << generateOrderNum();
     strstream >> oNum;
     return oNum;
 }
- //ALLOWS PROGRAM TO BE ABLE TO COUNT LINES IN THE FILES 
-int countFileLines(string holder)
-{
-    int count;
-   
-    fstream inFile;
-    inFile.open(holder);
-       while (getline(inFile, cust))  //creates a count of items to create pointer array
-        count++;
-    return count;
-    
-    inFile.close();
-    }
-//INCREASE FILE COMPACITY AS IT GROWS
 
 //ASSIGNS CUSTOMER ADDRESS DEPENDING ON THE INDEX 
 void AddressVectorAssign(){
    for(int x=0; x < mAddress.size(); x++){
        vector<Address>::iterator addA= addVec.begin();
        advance(addA, mAddress[x]);
-       (*(custId+x)).setCusAddress(&(*addA));
+       custId[x].setCusAddress(&(*addA));
    }
 }
 
@@ -90,88 +94,99 @@ int AddAddress(string _streetAddress, string _city, string _state, string _zipCo
     return addVec.size()-1;
 }
 
+
 void getCustumerInfo(int count, string f){
     //COLLECTS INFO AND PARSES IT
     fstream infile;
     infile.open(f);
     
-    for(int x=0; x< count; x++ ){
-        getline(infile, cust);
-        
-        CNum=  StringHelper::parse(cust,'|')[0];
-        CName= StringHelper::parse(cust,'|')[1];
-        CreditL=stod(StringHelper::parse(cust,'|')[2]);
-        _addy= StringHelper::parse(cust,'|')[3];
-        _streetAddress= StringHelper::parse(addy,',')[0];
-        _city= StringHelper::parse(addy,',')[1];
-        _state= StringHelper::parse(addy,',')[2];
-        _zipCode= StringHelper::parse(addy,',')[3];
-        
-        (*(custId+x)).setcusName(CName);
-        (*(custId+x)).setcusNum(CNum);
-        (*(custId+x)).setlCredit(CreditL);
-        (*(addressId+x)).setCity(_city);
-        (*(addressId+x)).setState(_state);
-        (*(addressId+x)).setZipCode(_zipCode);
-        
-        mAddress.push_back(AddAddress(_streetAddress, _city, _state, _zipCode));
-        AddressVectorAssign();
-        
-    }
+    vector<string> cust;
+    vector<string> addr;
+    string temp;
     
+    for(int x=0; x< count; x++ ){
+        getline(infile, temp);
+        //cout << temp << endl;
+        cust = StringHelper::parse(temp, '|');
+        //cout << cust[0] << endl;
+        CNum=  cust[0];
+        CName= cust[1];
+        CreditL = stod(cust[2]);
+        _addy= cust[3];
+        
+         addr = StringHelper::parse(_addy, ',');
+        
+        _streetAddress= addr[0];
+        _city= addr[1];
+        _state= addr[2];
+        _zipCode= addr[3];
+        
+        custId[x].setcusName(CName);
+        custId[x].setcusNum(CNum);
+        custId[x].setlCredit(CreditL);
+        addressId[x].setCity(_city);
+        addressId[x].setState(_state);
+        addressId[x].setZipCode(_zipCode);
+        
+         mAddress.push_back(AddAddress(_streetAddress, _city, _state, _zipCode));
+         AddressVectorAssign();
+   }  
 }
 
-int getCustInfo(string id)
+int findCustInfo(string id)
 {
+   int exit;
    for(int i=0; i < fileCount; i++)
    {
-       if((*(custId+i)).getcusNum() == id)
+       if(custId[i].getcusNum() == id)
        {
           return i;
        }
-       return -1;
-            
-          
-    
+       else
+          exit = -1;
+   }
+    return exit;
 }
-}
+
 
 
 int main(){
+    cout << "Start" << endl;
     int custIndex;
-    double total = 0;
     string associate;
     string cID;
+    double total = 0;
+    int counts= fileCount;
+  
+    getCustumerInfo(counts, "customers.dat");
+
     //OPENS UP ORDER SUMMARY
     ofstream orderSummary;
     orderSummary.open("orderSummary.dat"); 
-        
-    getCustumerInfo(fileCount,"customer.dat");
+       
     orderSummary <<"Order Number: " << generateOrderNum()<<endl;
     string NumOrder= generateOrderNum();
+    
+    
+
     cout <<"Enter Associate's Name: "<<endl;
     cin >>associate;
     
-    fileCount = countFileLines("customers.dat");
-    setPointers(fileCount);
-    do{
-    //CORPERATE NUM VALIDATION
-   
- 
-  
-    cout <<"Enter Customer ID:" <<endl;
-      cin  >> cID;
-      custIndex = getCustInfo(cID);
     
+    //CORPERATE NUM VALIDATION   
+
+    cout <<"Enter Customer ID:" <<endl;
+    cin  >> cID;
+    custIndex = findCustInfo(cID);
     if(custIndex == -1){
         cout << "Invalid Customer Id Number" << endl;
     }
     else
     {
-        orderSummary << "Customer Number:" << (*(custId+getCustInfo(cID))).getcusNum() << endl;
-        orderSummary << "Customer: " << (*(custId+getCustInfo(cID))).getcusName() << endl;
+        orderSummary << "Customer Number:" << (*(custId+findCustInfo(cID))).getcusNum() << endl;
+        orderSummary << "Customer: " << (*(custId+findCustInfo(cID))).getcusName() << endl;
     }
- }while (custIndex == -1);
+  
      int iNum, iQTY,count = 0, iNumChoice, j, itmcount;
      double iPrice;
      string iDescrip, holder1;
@@ -247,23 +262,26 @@ int main(){
                   
     //VALIDATION FOR BALANCE OVERDRAFT 
                }
-                total+= (purchaseQTY[i]*(*(productInfo+i)).getPrice());
+               double itemTotal = (purchaseQTY[i]*(*(productInfo+i)).getPrice());
+               total+= itemTotal;
                 double creditCheck;
-                creditCheck=(*(custId+getCustInfo(cID))).getlCredit();
+                creditCheck=(*(custId+findCustInfo(cID))).getlCredit();
                 if (creditCheck<total){
                     cout << "Not enough credit to purchase item" << endl;
                     bool flag=false;
                     (*(productInfo+i)).setBool(flag);
+                    purchaseQTY[i]=0;
+                    total-=itemTotal;
                 }
                 int newStockQTY;
-                newStockQTY = (*(productInfo+i)).getStockQuantity() - qty;
+                newStockQTY = (*(productInfo+i)).getStockQuantity() - purchaseQTY[i];
                 (*(productInfo+i)).setStockQuantity(newStockQTY);
                 error1=true;
-            }
-            else if (iNumChoice==0){
+              }
+              else if (iNumChoice==0){
                  choice1=true;
-                error1=true;
-             }
+                 error1=true;
+              }
             i++;
         }
         if (error1==false){
@@ -274,18 +292,21 @@ int main(){
     
     invFile.close();
     invFile.clear(); //closes and clears input file;
-    
+    Address * ap;
+    ap =(*(custId+findCustInfo(cID))).getCusAddress();
      //RECIEPT
     cout <<"-------------------------------------------------------------------------" << endl;
     cout <<left << "B2B Shopping Cart" << endl;   
     cout <<"-------------------------------------------------------------------------" << endl; 
     cout <<left <<"Order Number: " << NumOrder <<endl;
     cout <<left <<"Associate: " << associate <<endl;
-    cout <<left <<"Customer Number: "<< (*(custId+getCustInfo(cID))).getcusNum() << endl;
-    cout <<left <<"Customer: " << (*(custId+getCustInfo(cID))).getcusName() << endl;
-    cout <<left <<"Address: "<< custId <<endl;
+    cout <<left <<"Customer Number: "<< (*(custId+findCustInfo(cID))).getcusNum() << endl;
+    cout <<left <<"Customer: " << (*(custId+findCustInfo(cID))).getcusName() << endl;
+    cout <<left <<"Address: "; 
+    ap->getAddress();
+    cout << endl;
  
-    cout <<setw(10) << "Item No " << setw(10) << "Description " << setw(10) << "Qty" << setw(10) << "Total" <<endl;
+    cout <<setw(20) << "Item No " << setw(20) << "Description " << setw(20) << "Qty" << setw(20) << "Total" <<endl;
     cout <<"-------------------------------------------------------------------------" << endl; 
        
     while(choice2==false){ //creates output for final order summary
@@ -301,20 +322,43 @@ int main(){
                 qty=(*(productInfo+i)).getStockQuantity();
                 pTotal=purchaseQTY[i]*p;
                  orderSummary << num << " " << descr <<" "<<purchaseQTY[i]<<" $"<< pTotal<<endl; // needs setprecision[2]
-                 cout << setw(10) << num << " " << descr <<" "<<purchaseQTY[i]<<" $"<< pTotal<<endl;
-           
-                              }
+                 cout << setw(30) << num << " " << descr <<" "<<purchaseQTY[i]<<" $"<< pTotal <<endl;
+        
+                   }
             i++;
          }
          choice2=true; 
-    }
+         double newCredit = (*(custId+findCustInfo(cID))).getlCredit() - total;
+    (*(custId+findCustInfo(cID))).setlCredit(newCredit);                     
      cout <<"-------------------------------------------------------------------------" << endl; 
      orderSummary << "$" << total << endl; // prints total
-     cout <<left << "Total" << setw(10) << "$" << total << endl;
+     cout <<left << "Total" << setw(30) << "$" << total << endl;
      cout <<"-------------------------------------------------------------------------" << endl; 
-     cout <<left << "Remaning Credit" << setw(10) << endl;
-    double newCredit = (*(custId+getCustInfo(cID))).getlCredit() - total;
-    (*(custId+getCustInfo(cID))).setlCredit(newCredit);
-    
+     cout <<left << "Remaning Credit" << setw(10) << " " << newCredit << endl;
+    }
+    ofstream custEdit;
+    custEdit.open("customers.dat");
+      for(int q= 0; q< fileCount; q++){
+        ap =(*(custId+q)).getCusAddress();
+        
+        custEdit << custId[q].getcusNum() << "|"
+        << custId[q].getcusName()  << "|"
+        << custId[q].getlCredit() << "|"
+        << ap->getStreetAddress()  << ","
+        << ap->getCity()  << ","
+        << ap->getState()  << ","
+        << ap->getZipCode();
+        if (q<(fileCount-1))
+            custEdit << endl;
+    }
+  invFile.open("inventory.dat");
+        for(int p= 0; p< fileCount; p++){
+        invFile << (*(productInfo+p)).getItemNo()  << ","
+         << (*(productInfo+p)).getDescription()  << ","
+         << (*(productInfo+p)).getPrice()  << ","
+         << (*(productInfo+p)).getStockQuantity();
+         if (p<(fileCount-1))
+             invFile << endl;
+    }
  	return 0;
 }
